58663980666a7a56fac9328462eabe7ff48c6705
CíborgAscend <lizz_gonzalez@hotmail.com>
2015-03-20 22:18:08 -0600

    primercommit

---
+++ Ejercicios php guia 2/1.php
@@ -0,0 +1,14 @@
+﻿ <<p>foreach con arreglo de una dimensión de dirreccion</p> 
+ 
+ <?php 
+ $arreglo=array('calle'=>'los almendros',  
+ 'colonia'=>'Col. las brisas',  
+ 'numero de casa'=>'12'); 
+ 'municipio'=>'San Rafael Obrajuelo'); 
+ foreach ($arreglo as $dato) { print $dato."<br>"; 
+ 
+ }  
+ 
+ ?> 
+  <p>foreach con arreglo de multidimensión</p> 
+ <?php 
---
+++ Ejercicios php guia 2/direccion.php
@@ -0,0 +1,18 @@
+﻿<?php
+$comentario;
+$calle;
+$colonia;
+$numero;
+$municipio;
+$departamento;
+
+{
+print $comentario = 'Mi direccion es:';
+print $calla = 'calle los almendros,';
+print $cononia= ' Col brisas,';
+print $numero= 'Casa nª 12,';
+print $municipio= 'Municipio de san rafael obrajuelo,';
+print $departamento= ' Departamento la paz,';
+
+}
+?>
